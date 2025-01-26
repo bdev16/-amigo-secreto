f0ad79837b34de8dce371806f28c7dd651f2ae6f
DevBruno <brunoantonio.dev0016@gmail.com>
2025-01-26 15:28:44 -0300

    feat(app.js): Criação da função adicionarAmigo.
    Esse commit tem o intuito de criar a função adicionarAmigo, que vai ser
    utilizada quando o usuario informar um valor e clicar no botao de
    adicionar presente no HTML. Essa função vai pegar o valor informado e
    atribuir a lista de amigos

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let friends = [];
 
+function adicionarAmigo() {
+    let nomeAmigo =  document.querySelector('input').value
+    let isValid = validaEntrada(nomeAmigo);
+
+    if (!isValid) {
+        alert("Por favor, insira um nome.");
+    } else {
+        friends.push(nomeAmigo);
+        nomeAmigo = document.querySelector('input');
+        nomeAmigo.value = '';
+        console.log(friends);
+    }
+}
+
 function validaEntrada(nomeAmigo) {
     if (!nomeAmigo) {
         return false;
